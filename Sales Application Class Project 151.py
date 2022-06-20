24989dc43b5fca7615883f7fbfe9711bc9be210e
SwastiAK <85284257+SwastiAK@users.noreply.github.com>
2022-06-20 18:48:55 +0530

    Add files via upload

--- Sales Application Class Project 151.py
+++ Sales Application Class Project 151.py
@@ -6,7 +6,6 @@ Created on Sat Jun 18 16:53:17 2022
 """
 
 from  tkinter import *
-import random
 
 root = Tk()
 root.title("Sales Application")
@@ -23,20 +22,20 @@ month = ("January", "February", "March", "April", "May", "June", "July", "August
 
 profits = (20000, 45983, 45620, 47103, 50000, 490000, 47653, 32100, 78600, 80000, 132650, 45698)
 
-label_month = ["text"] = "Months : " + str(month)
-label_profit = ["text"] = "Profits : " + str(profits)
+label_month ["text"] = "Months : " + str(month)
+label_profit ["text"] = "Profits : " + str(profits)
 
 def maxProfit():
     max_profit = max(profits)
-    max_profit_index = profit.index(max_profit)
-    max_profit_month = profit.month[max_profit_index]
-    print("The Maximum Profit Of " + str(max_profit) + " Was Recorded In The Month Of " + str(max_profit_month))
+    max_profit_index = profits.index(max_profit)
+    max_profit_month = month[max_profit_index]
+    label_max_profit["text"] = "The Maximum Profit Of " + str(max_profit) + " Was Recorded In The Month Of " + str(max_profit_month)
     
 def minProfit():
     min_profit = min(profits)
-    min_profit_index = profit.index(min_profit)
-    min_profit_month = profit.month[miin_profit_index]
-    print("The Minimum Profit Of " + str(min_profit) + " Was Recorded In The Month Of " + str(min_profit_month))
+    min_profit_index = profits.index(min_profit)
+    min_profit_month = month[min_profit_index]
+    label_min_profit ["text"] = "The Minimum Profit Of " + str(min_profit) + " Was Recorded In The Month Of " + str(min_profit_month)
     
 label_month.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 label_profit.place(relx = 0.5, rely = 0.3, anchor = CENTER)
